d36d0e0e5fb29ab423e95ea763e8d843b1cc8d7f
Dick Visser <dick.visser@geant.org>
2023-09-21 16:03:30 +0200

    hard code

--- src/Auth/Process/MellonTruncate.php
+++ src/Auth/Process/MellonTruncate.php
@@ -23,24 +23,21 @@ class MellonTruncate extends Auth\ProcessingFilter {
 
         $mapped_attributes = [];  // FIXME do we need this?
 
-        $value_max_length = 383;
-        $name_max_length = 127;
-
         foreach ($state['Attributes'] as $name => $values) {
 
             if (strlen($name) > $name_max_length) {
-                Logger::debug("Attribute name '$name' too long, truncating it to $name_max_length chars");
-                $newname = substr($name, $name_max_length);
+                Logger::debug("Attribute name '$name' too long, truncating it to 127 chars");
+                $newname = substr($name, 127);
                 $request['Attributes'][$newname] = $values;
                 unset($request['Attributes'][$name]);
             }
 
-            $too_long_values = array_filter($values, function($x) { return strlen($x) > $value_max_length; });
+            $too_long_values = array_filter($values, function($x) { return strlen($x) > 383; });
             foreach ($too_long_values as $tl) {
-                Logger::debug("Attribute value '$tl' too long, truncating it to $value_max_length");
+                Logger::debug("Attribute value '$tl' too long, truncating it to 383");
             }
             if(count($too_long_values) > 0) {
-                $request['Attributes'][$name] = array_map(function($x) {return substr($x, 0, $value_max_length); }, $values);
+                $request['Attributes'][$name] = array_map(function($x) {return substr($x, 0, 383); }, $values);
             }
         }
     }
