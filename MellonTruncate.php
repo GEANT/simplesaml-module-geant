0913a953ab6a6bc8242c3dbb2235942a066dddce
Dick Visser <dick.visser@geant.org>
2023-09-21 16:22:25 +0200

    Fixes

---
+++ MellonTruncate.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Module\geant\Auth\Process;
+
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\Auth;
+use SimpleSAML\Error;
+use SimpleSAML\Logger;
+
+/**
+ * Truncate attribute names and values for use with mod_mellon
+ *
+ * @package SimpleSAMLphp
+ */
+
+class MellonTruncate extends Auth\ProcessingFilter {
+
+    public function process(array &$state): void {
+
+        Assert::keyExists($state, 'Attributes');
+
+        $mapped_attributes = [];  // FIXME do we need this?
+
+        foreach ($state['Attributes'] as $name => $values) {
+
+            if (strlen($name) > 12) {
+                Logger::debug("Attribute name '$name' too long, truncating it to 12 chars");
+                $newname = substr($name, 0, 12);
+                $state['Attributes'][$newname] = $values;
+                unset($state['Attributes'][$name]);
+            }
+
+            $too_long_values = array_filter($values, function($x) { return strlen($x) > 383; });
+
+            foreach ($too_long_values as $tl) {
+                Logger::debug("Attribute value '$tl' too long, truncating it to 383");
+            }
+            if(count($too_long_values) > 0) {
+                $state['Attributes'][$name] = array_map(function($x) {return substr($x, 0, 383); }, $values);
+            }
+        }
+    }
+}
